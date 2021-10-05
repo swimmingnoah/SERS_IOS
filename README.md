b7d9ad1ace76c6d2a5ba5d4f830de182bea011e7
swimmingnoah <noah@nemecfamily.com>
2021-10-05 10:15:47 -0500

    {1Hr} implemented started map and added new views for map and tips. also started readme

---
+++ README.md
@@ -0,0 +1,4 @@
+#  Links for helping with mapview
+- https://www.youtube.com/watch?v=f6xN2MuHv1s
+- https://www.raywenderlich.com/7738344-mapkit-tutorial-getting-started
+
---
+++ SERS/Controllers/MapViewController.swift
@@ -0,0 +1,52 @@
+//
+//  MapViewController.swift
+//  SERS
+//
+//  Created by Noah Nemec on 10/4/21.
+//
+
+import UIKit
+import RealmSwift
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+	@IBOutlet var MapView: MKMapView!
+	
+	let manager = CLLocationManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+		
+		
+    }
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		manager.delegate = self
+		manager.desiredAccuracy = kCLLocationAccuracyBest
+		manager.requestWhenInUseAuthorization()
+		manager.startUpdatingLocation()
+	}
+	
+	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+		if let location = locations.first {
+			manager.stopUpdatingLocation()
+			
+			render(location)
+		}
+	}
+	func render(_ location: CLLocation){
+		let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude,
+												longitude: location.coordinate.longitude)
+		
+		let span = MKCoordinateSpan(latitudeDelta: 0.1,
+									longitudeDelta: 0.1)
+		
+		let region = MKCoordinateRegion(center: coordinate,
+										span: span)
+		MapView.setRegion(region,
+						  animated: true)
+	}
+    
+}
