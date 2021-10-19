6784d6e79196d1bb9f2695daa9fbd0c3b9488f0e
Kyle Rohlfing <kylerohlfing27@gmail.com>
2021-10-19 13:35:23 -0500

    added storyboard

--- SERS/Controllers/TipLineViewController.swift
+++ SERS/Controllers/TipLineViewController.swift
@@ -11,6 +11,7 @@ class TipLineViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        view.backgroundColor = .lightGray
 
         // Do any additional setup after loading the view.
     }
