d29e2c440055c9dc8474a6c2ed0308f351af2d93
Kyle Rohlfing <kylerohlfing27@gmail.com>
2021-10-19 13:10:26 -0500

    Initial Commit

---
+++ SERS/Controllers/TipLineViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TipLineViewController.swift
+//  SERS
+//
+//  Created by Kyle Rohlfing on 10/19/21.
+//
+
+import UIKit
+
+class TipLineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
