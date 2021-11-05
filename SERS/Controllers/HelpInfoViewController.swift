f5e8056d94bec967a3ac602de0c8bd040ef34e37
Ryan Woods <rsw7vc@umsystem.edu>
2021-11-04 22:00:04 -0500

    Text descriptions for help pages done

--- SERS/Controllers/HelpInfoViewController.swift
+++ SERS/Controllers/HelpInfoViewController.swift
@@ -8,18 +8,18 @@
 //
 
 import UIKit
+import SwiftUI
 
 class HelpInfoViewController: UIViewController {
 
     @IBOutlet weak var infoLabel: UILabel!
     
     var info: String?
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
         infoLabel.text = info
-
+        infoLabel.numberOfLines = 0
         // Do any additional setup after loading the view.
     }
     
