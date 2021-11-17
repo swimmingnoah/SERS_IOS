0e21109b8d2f55ee978824e717aee0c620156219
swimmingnoah <54280709+swimmingnoah@users.noreply.github.com>
2021-11-16 22:48:23 -0600

    Revert "Revert "Data submission""

---
+++ SERS/Controllers/NewProfileViewController.swift
@@ -0,0 +1,47 @@
+//
+//  NewProfileViewController.swift
+//  SERS
+//
+//  Created by Kyle Rohlfing on 11/11/21.
+//
+
+import UIKit
+import Firebase
+
+class NewProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBOutlet weak var fnameField: UITextField!
+    @IBOutlet weak var lnameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var phoneNumField: UITextField!
+    
+    
+    
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        print("Submit button pressed!")
+        
+//        var ref: DatabaseReference!
+//        ref = Database.database().reference()
+//        let userID = Auth.auth().currentUser?.uid
+        
+//        let fname = fnameField.text!
+//        let lname = lnameField.text!
+//        let email = emailField.text!
+//        let phoneNum = phoneNumField.text!
+        
+        let userData = [
+            "fname": fnameField.text!,
+            "lname": lnameField.text!,
+            "email": emailField.text!,
+            "phoneNum": phoneNumField.text!
+        ]
+        
+        print(userData)
+        
+    }
+
+}
